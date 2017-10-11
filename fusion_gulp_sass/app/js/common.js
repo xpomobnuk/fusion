0f0ea6d19dc4d3a1e90740e819fc6f0818886d50
Vladimir <xpomobnuk@gmail.com>
2017-10-11 15:35:19 +0300

    old pc

--- fusion_gulp_sass/app/js/common.js
+++ fusion_gulp_sass/app/js/common.js
@@ -6,17 +6,12 @@ function load()	{
 	$(".loader_inner").fadeOut();
 	$(".loader").delay(300).fadeOut("slow");
 };
-
-$(".top_text h1").animated("fadeInDown", "fadeOutUp")
-$(".top_text ul").animated("fadeInUp", "fadeOutDown")
-
 load();
 
-function heightDetect(){
-		$(".main_head").css("height", $(window).height());
-	};
+$(".top_text h1").animated("fadeInDown", "fadeOutUp");
+$(".top_text ul").animated("fadeInUp", "fadeOutDown");
 
-heightDetect();
+
 
 
 
